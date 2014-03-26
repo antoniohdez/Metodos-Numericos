c9d6cb481eaa47152e9ca79c8b28da76e4139b90
Antonio <antonio.hdez93@gmail.com>
2014-03-26 16:41:50 -0600

    Método para resolver por Lagrange

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 import csv
 
 def readFile(filename = "datos.txt"):
@@ -7,10 +8,11 @@ def readFile(filename = "datos.txt"):
 			n = int(next(reader)[0])
 			datos = [[0 for x in range(2)] for x in range(n)]
 			x = y = 0
+			print("{:10} {:10}".format("X","Y"))
 			for row in reader:
 				for val in row:
 					datos[x][y] = int(val)
-					print("{}".format(val), end=" ")
+					print("{:10}".format(val), end=" ")
 					y += 1
 				print("")
 				x += 1
@@ -19,5 +21,33 @@ def readFile(filename = "datos.txt"):
 		except csv.Error as e:
 			sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 	return datos
+
+def Lagrange(datos, valor):
+	n =	len(datos)
+	A=[Fraction(1) for x in range(n)]
+
+	for x in range(n):
+		divisor = 1
+		for xi in range(n):
+			if xi != x:
+				divisor *= datos[x][0] - datos[xi][0]
+		A[x] = Fraction(datos[x][1])/Fraction(divisor)
+
+	resultado = 0
+	for x in range(n):
+		multi = 1
+		for xi in range(n):
+			if xi != x:
+				multi *= valor - datos[xi][0]
+		resultado += A[x] * Fraction(multi)	
+	print()
+	print("El valor interpolado de Y para el valor X = {} es: {}".format(valor, resultado))
+
+#y(x) =  A0 = (x-x1)(x-x2)(x-x3)+
+#		A1 (x-x0)(x-x2)(x-x3)+
+#		A2 (x-x0)(x-x1)(x-x3)+
+#		A3 (x-x0)(x-x1)(x-x2);
+
 	
 datos = readFile()
+Lagrange(datos, 2)
