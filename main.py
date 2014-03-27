3832d359763334b3f58e07582b0ecb4a5a9bb532
Antonio <antonio.hdez93@gmail.com>
2014-03-26 23:42:30 -0600

    Método de newton (incompleto)
    Validación para saber si se puede utilizar el método (incremento
    constante en x), cálculo de los valores de deltaY.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fractions import Fraction
 import csv
 
-def readFile(filename = "datos.txt"):
+def readFile(filename = "newton.txt"):
 	with open(filename,'r') as file:
 		try:
 			reader = csv.reader(file)
@@ -24,8 +24,7 @@ def readFile(filename = "datos.txt"):
 
 def Lagrange(datos, valor):
 	n =	len(datos)
-	A=[Fraction(1) for x in range(n)]
-
+	A=[1 for x in range(n)]
 	for x in range(n):
 		divisor = 1
 		for xi in range(n):
@@ -40,14 +39,38 @@ def Lagrange(datos, valor):
 			if xi != x:
 				multi *= valor - datos[xi][0]
 		resultado += A[x] * Fraction(multi)	
+	
 	print()
 	print("El valor interpolado de Y para el valor X = {} es: {}".format(valor, resultado))
+	return resultado
 
-#y(x) =  A0 = (x-x1)(x-x2)(x-x3)+
-#		A1 (x-x0)(x-x2)(x-x3)+
-#		A2 (x-x0)(x-x1)(x-x3)+
-#		A3 (x-x0)(x-x1)(x-x2);
+def Newton(datos, valor, orden):
+	n =	len(datos)
+	resultado = 0
+	diferencia = datos[1][0] - datos[0][0]
+	for x in range(1, n):	
+		if diferencia != (datos[x][0] - datos[x-1][0]):
+			return Lagrange(datos, valor)
+
+	if orden >= n:
+		return Lagrange(datos, valor)
+	deltaY = []
+	for x in range(orden-1):
+		deltaY.append( [0 for x in range(n-(x + 1))] )
+		if x == 0:
+			for xi in range(len(deltaY[0])):
+				deltaY[0][xi] = datos[xi+1][1] - datos[xi][1]
+				print(deltaY[0][xi], end=" ")
+			print()
+		else:
+			for xi in range(len(deltaY[x])):
+				deltaY[x][xi] = deltaY[x-1][xi+1] - deltaY[x-1][xi]
+				print(deltaY[x][xi], end=" ")
+			print()
+
+	return resultado
 
 	
 datos = readFile()
-Lagrange(datos, 2)
+#Lagrange(datos, 2)
+Newton(datos, 3.2, 4)
